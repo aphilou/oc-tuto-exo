cf890cc8ca25b88031e39d5ec97476909e2e2c04
Philippe Yo <poub1@neuf.fr>
2019-03-17 11:39:06 +0100

    Observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { AppareilService } from './services/appareil.service';
+import { interval } from 'rxjs';
+import 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +8,24 @@ import { AppareilService } from './services/appareil.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
+
+  secondes: number;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    const counter = interval(1000);
+    counter.subscribe(
+      (value: number) => {
+        this.secondes = value;
+      },
+      (error: any) => {
+        console.error('Erreur détectée: ', error);
+      },
+      () => {
+        console.log('Observable counter terminé !');
+      }
+    )
+  }
 
 }
